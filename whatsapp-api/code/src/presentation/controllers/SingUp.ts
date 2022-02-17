a7801d3ee8ec63ae6232e207a3bbc6803c286b07
Rafael Lessa <rafael.lessa@basesoft.com.br>
2022-02-17 15:39:47 -0300

    feat: create a signup controller

---
+++ whatsapp-api/code/src/presentation/controllers/SingUp.ts
@@ -0,0 +1,48 @@
+import { AddAcount } from '../../core/usecases/add-account'
+import { Controller, EmailValidator, HttpRequest, HttpResponse } from '../protocols'
+
+import { badRequest, serverError, successCreatedRequest } from '../helpers/http-helper'
+import { MissingParamError } from '../errors/missing-param-error'
+import { InvalidParamError } from '../errors/invalid-param-error'
+
+export class SignUpController implements Controller {
+  private readonly emailValidator: EmailValidator
+  private readonly addAccount: AddAcount
+  constructor (emailValidator: EmailValidator, addAccount: AddAcount) {
+    this.emailValidator = emailValidator
+    this.addAccount = addAccount
+  }
+
+  handle (httpRequest: HttpRequest): HttpResponse {
+    try {
+      const requiredFields = [
+        'name',
+        'email',
+        'password',
+        'passwordConfirmation'
+      ]
+
+      const { name, email, password, passwordConfirmation } = httpRequest.body
+
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
+      }
+
+      if (password !== passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
+      }
+
+      if (!this.emailValidator.isValid(email)) {
+        return badRequest(new InvalidParamError('email'))
+      }
+
+      this.addAccount.handle({ name, email, password })
+
+      return successCreatedRequest(httpRequest.body)
+    } catch (error) {
+      return serverError()
+    }
+  }
+}
